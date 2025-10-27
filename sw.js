29177e1cabebd5003e3c763813ba25c151029c6a
GARAGANDA <152786014+GARAGANDA@users.noreply.github.com>
2025-10-27 17:03:07 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-  './', // Кешируем корень для офлайн-доступа
+  './', 
   'index.html',
   'app.js',
   'images/icon.png',
@@ -9,34 +9,34 @@ const assets = [
   'images/icons/icon_192.png'
 ];
 
-// install event
+
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
-      return cache.addAll(assets); // Добавляем return, чтобы Promise разрешился
+      return cache.addAll(assets); 
     })
   );
 });
 
-// activate event
+
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys
-        .filter(key => key !== staticCacheName) // Используем !== вместо !=
+        .filter(key => key !== staticCacheName)
         .map(key => caches.delete(key))
       );
     })
   );
 });
 
-// fetch event
+
 self.addEventListener('fetch', evt => {
     
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request); // Возвращаем результат fetch()
+      return cacheRes || fetch(evt.request);
     })
   );
 });
